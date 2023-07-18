49f31c6eb3128835c8112e9b788868ae2e3a941b
divyasripoloju <139854201+divyasripoloju@users.noreply.github.com>
2023-07-18 13:27:32 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,12 @@
+lst=list(map(int, input().split(',')))
+a=[1,2,3,4,5,6,7,8,9]
+dit={}
+
+for i in a:
+   count=0;
+   for j in lst:
+      if(j%i==0):
+         count+=1
+   dit[i]=count
+        
+print(dit)
